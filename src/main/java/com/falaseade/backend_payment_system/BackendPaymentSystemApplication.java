b9b3be8439c6545eef5d197d83e9197f2c9f8140
TF <tfalase24@gmail.com>
2025-07-17 14:10:38 +0100

    First Commit

---
+++ src/main/java/com/falaseade/backend_payment_system/BackendPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.falaseade.backend_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falaseade/backend_payment_system/BackendPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falaseade.backend_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
